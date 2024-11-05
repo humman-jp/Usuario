37c8966ef82c07d3b051e7651e999f79283e1ae1
andrea-yamileth <andreachaquinga2005@gmail.com>
2024-11-05 15:16:48 -0500

    hola usuario

---
+++ src/main/java/ec/edu/espoch/usuario/Usuario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.usuario;
+
+/**
+ *
+ * @author core i3
+ */
+public class Usuario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
